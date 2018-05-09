e21bbdcb4aed2f78dd407be52380d69649acb39e
Aleksandar Pavlovic <alex.pavlovic92@gmailc.om>
2018-05-09 21:23:28 +0200

    implemented suffix array downsampling

--- bwt_fmindex_search/fmindex_optimized.py
+++ bwt_fmindex_search/fmindex_optimized.py
@@ -7,10 +7,10 @@ import argparse
 terminal_char = '\0'
 
 
-def bw_transform(text, suffix_array):
+def bw_transform(text, sa):
     """ Returns BWT(text) """
     bw = []
-    for si in suffix_array:
+    for si in sa.array:
         if si == 0:
             bw.append(terminal_char)
         else:
@@ -39,17 +39,27 @@ def terminate_string(s):
     return s if s[-1:] == terminal_char else s + terminal_char
 
 
-def query(bwt, counts, tally, suffix_array, pattern):
+def query(bwt, f_column, tally, suffix_array, pattern):
     reverse_pattern = pattern[::-1]
-    start_index, end_index = char_range(counts, reverse_pattern[0])
+    start_index, end_index = f_column.char_range(reverse_pattern[0])
+    if start_index == end_index:
+        return []
     for c in reverse_pattern[1:]:
         first_rank, count = find_preceders(bwt, start_index, end_index, tally, c)
         if count == 0:
             return []
         else:
-            start_index = find_first_occurrence(counts, c) + first_rank
+            start_index = f_column.first_occurrence(c) + first_rank
             end_index = start_index + count
-    return suffix_array[start_index:end_index]
+    return [find_suffix(suffix_array, bwt, tally, f_column, i) for i in range(start_index, end_index)]
+
+
+def find_suffix(sa, bwt, tally, f_column, index):
+    if index % sa.factor == 0:
+        return sa.array[index // sa.factor]
+    else:
+        rank = find_tally(bwt, index, tally, bwt[index]) - 1
+        return 1 + find_suffix(sa, bwt, tally, f_column.first_occurrence(bwt[index]) + rank)
 
 
 def find_preceders(bwt, start_index, end_index, tally, c):
@@ -82,15 +92,13 @@ def round_up(n, base):
     return (n + base - 1) // base * base
 
 
-def char_range(counts, char):
-    """ Returns range of indices in F array inside which char is contained.
-     The range is in form [start, end) i.e. does not include end index """
-    start_index = find_first_occurrence(counts, char)
-    return start_index, start_index + counts[char]
-
-
-def find_first_occurrence(counts, char):
-    return sum(count[1] for count in counts.items() if count[0] < char)
+def calculate_first_occurrences(counts):
+    first_occurrences = {}
+    s = 0
+    for k, v in sorted(counts.items()):
+        first_occurrences[k] = s
+        s += v
+    return first_occurrences
 
 
 def create_ranks_tally(bwt, tally_factor):
@@ -121,16 +129,33 @@ class Tally:
         self.ranks = create_ranks_tally(bwt, factor)
 
 
+class SuffixArray:
+    def __init__(self, text, factor):
+        self.factor = factor
+        self.array = suffix_array_manber_myers(text)[::factor]
+
+class FColumn:
+    def __init__(self, text):
+        self.counts = count_characters(text)
+        self.zero_rank_indices = calculate_first_occurrences(self.counts)
+
+    def char_range(self, c):
+        return self.first_occurrence(c), self.first_occurrence(c) + self.counts[c]
+
+    def first_occurrence(self, c):
+        return self.zero_rank_indices.get(c, 0)
+
+
 class FMIndex:
     def __init__(self, text, sa_factor=1, tally_factor=1):
         text_terminated = terminate_string(text)
-        self.char_count = count_characters(text_terminated)
-        self.suffix_array = suffix_array_manber_myers(text_terminated)
+        self.f_column = FColumn(text_terminated)
+        self.suffix_array = SuffixArray(text_terminated, sa_factor)
         self.bwt = bw_transform(text_terminated, self.suffix_array)
         self.tally = Tally(self.bwt, tally_factor)
 
     def query(self, pattern):
-        return query(self.bwt, self.char_count, self.tally, self.suffix_array, pattern)
+        return query(self.bwt, self.f_column, self.tally, self.suffix_array, pattern)
 
 
 if __name__ == "__main__":
