41bf148a817529bed92af744b2183700557195e2
Aleksandar Pavlovic <alex.pavlovic92@gmailc.om>
2018-05-26 09:02:03 +0200

    another approach to suffix array creation

--- bwt_fmindex_search/sa.py
+++ bwt_fmindex_search/sa.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from itertools import zip_longest, islice
 
 
 def suffix_array_naive(text):
@@ -92,6 +93,48 @@ def suffix_array_quicksort(text):
     sa = list(range(len(text)))
     quick_sort(sa, text, 0, len(text) - 1)
     return sa
+
+
+def to_int_keys_best(l):
+    """
+    l: iterable of keys
+    returns: a list with integer keys
+    """
+    seen = set()
+    ls = []
+    for e in l:
+        if not e in seen:
+            ls.append(e)
+            seen.add(e)
+    ls.sort()
+    index = {v: i for i, v in enumerate(ls)}
+    return [index[v] for v in l]
+
+
+def inverse_array(l):
+    n = len(l)
+    ans = [0] * n
+    for i in range(n):
+        ans[l[i]] = i
+    return ans
+
+
+def suffix_array_best(s):
+    """
+    suffix array of s
+    O(n * log(n)^2)
+    """
+    n = len(s)
+    k = 1
+    line = to_int_keys_best(s)
+    while max(line) < n - 1:
+        line = to_int_keys_best(
+            [a * (n + 1) + b + 1
+             for (a, b) in
+             zip_longest(line, islice(line, k, None),
+                         fillvalue=-1)])
+        k <<= 1
+    return inverse_array(line)
 
 
 if __name__ == "__main__":
